0c3061a63a7317909560dffdc40c59a4ef80de7d
Faybe90 <fai.behlouli@gmail.com>
2024-08-01 20:49:07 +0100

    commit

---
+++ middleware/auth.js
@@ -0,0 +1,6 @@
+export function verification(req, res, next) {
+  if (req.body.sessionId != "adgk126kjhgyrd986g&") {
+    return res.status(403).json({ message: "you are not allowed" });
+  }
+  next();
+}
---
+++ routers/pokimon.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+import {
+  createPokimon,
+  deletePokimon,
+  getAllPokimon,
+  getPokimonById,
+} from "../controllers/pokimon.js";
+import { verification } from "../middleware/auth.js";
+
+const pokimonRouters = express.Router();
+pokimonRouters.get("/", getAllPokimon);
+pokimonRouters.get(`/:id`, getPokimonById);
+pokimonRouters.post("/", verification, createPokimon);
+pokimonRouters.delete("/:id", verification, deletePokimon);
+export default pokimonRouters;
